bec9e161684372d54df4e2b91a9d7fb8e9dd749f
Yashika Vasantha <yashikav@infinite.com>
2024-04-29 13:32:07 +0530

    Assignments

---
+++ Infinite/Assessments/Web Technologies 2/MVC2/MVC2/Controllers/MoviesController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using MVC2.Models;
+
+namespace MVC2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MoviesDbContext db;
+
+        public MoviesController()
+        {
+            db = new MoviesDbContext();
+        }
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = db.Movies.ToList();
+            return View(movies);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        public ActionResult MoviesByYear(int year)
+        {
+            var movies = db.Movies.Where(m => m.DateOfRelease.Year == year).ToList();
+            return View(movies);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Infinite/Assessments/Web Technologies 2/MVC2/MVC2/Models/Movies.cs
+++ Infinite/Assessments/Web Technologies 2/MVC2/MVC2/Models/Movies.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity; // Add this line
 
 namespace MVC2.Models
 {
-    public class Movies
+    public class Movie
     {
         public int Mid { get; set; }
         public string MovieName { get; set; }
         public DateTime DateOfRelease { get; set; }
+    }
+
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext() : base("MoviesDbContext")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
     }
 }
